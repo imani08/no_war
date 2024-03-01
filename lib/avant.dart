f1fc47ac7e7bbebade9be3f050e3c2a3740a7743
imany <154815202+imani08@users.noreply.github.com>
2024-03-01 21:43:18 +0100

    ajout de messenger comminity

--- lib/avant.dart
+++ lib/avant.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:firebase_storage/firebase_storage.dart'; // Importer le package Firebase Storage
 import 'Ecran acceuil.dart';
 
 class Avant extends StatelessWidget {
@@ -20,33 +21,51 @@ class ImageCarouselPage extends StatefulWidget {
 class _ImageCarouselPageState extends State<ImageCarouselPage> {
   PageController _pageController = PageController(initialPage: 0);
   int _currentPage = 0;
-  List<String> _imagePaths = [
-    'assets/scroll/arial.jpg',
-    'assets/scroll/bool.jpg',
-    'assets/scroll/call.jpg',
-    'assets/scroll/mams.jpg',
-    'assets/scroll/sang.jpg',
-    'assets/scroll/congo.png',
-  ];
+  List<String> _imagePaths = [];
+
+  // Fonction pour récupérer les chemins des images depuis Firebase Storage
+  Future<void> _fetchImagePaths() async {
+    FirebaseStorage storage = FirebaseStorage.instance;
+    ListResult result = await storage.ref('scroll/').list();
+
+    setState(() {
+      _imagePaths = result.items.map((item) => item.fullPath).toList();
+    });
+  }
 
   @override
   void initState() {
     super.initState();
+    _fetchImagePaths(); // Récupérer les chemins des images depuis Firebase Storage
     _pageController.addListener(() {
       setState(() {
         _currentPage = _pageController.page!.round();
       });
       if (_currentPage == _imagePaths.length - 1) {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => EcranAccueil()),
+        // Afficher le bouton "Commencer" lorsque toutes les images sont affichées
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Commencer'),
+              content: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => EcranAccueil()),
+                  );
+                },
+                child: Text('Commencer'),
+              ),
+            );
+          },
         );
       }
     });
     _showNextImage(); // Appeler la fonction au lancement de la page
   }
+
   void _showNextImage() {
-    //  passer à l'image suivante
     Timer(Duration(seconds: 2), () {
       if (_currentPage < _imagePaths.length - 1) {
         _pageController.nextPage(
@@ -63,33 +82,67 @@ class _ImageCarouselPageState extends State<ImageCarouselPage> {
     _pageController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(
-        controller: _pageController,
-        itemCount: _imagePaths.length,
-        itemBuilder: (context, index) {
-          return Stack(
-            fit: StackFit.expand,
-            children: [
-              Image.asset(
-                _imagePaths[index],
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          // Image d'arrière-plan
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/background.jpg'), // Remplacer par le chemin de votre image d'arrière-plan
                 fit: BoxFit.cover,
               ),
-              Positioned(
-                left: 16,
-                bottom: 16,
-                child: Text(
-                  '→ →',
-                  style: TextStyle(fontSize: 25, color: Colors.white,fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ],
-          );
-        },
+            ),
+          ),
+          // GridView avec des images
+          GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
+            ),
+            itemCount: _imagePaths.length,
+            itemBuilder: (context, index) {
+              return Image.network(
+                _imagePaths[index],
+                fit: BoxFit.cover,
+              );
+            },
+          ),
+          // Indicateurs en bas
+          Positioned(
+            bottom: 16.0,
+            left: 0.0,
+            right: 0.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: _buildPageIndicator(),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  List<Widget> _buildPageIndicator() {
+    List<Widget> indicateurs = [];
+    for (int i = 0; i < _imagePaths.length; i++) {
+      indicateurs.add(
+        Container(
+          width: 8.0,
+          height: 8.0,
+          margin: EdgeInsets.symmetric(horizontal: 4.0),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: _currentPage == i ? Colors.white : Colors.grey,
+          ),
+        ),
+      );
+    }
+    return indicateurs;
   }
 }
--- lib/discussions.dart
+++ lib/discussions.dart
@@ -1,97 +1,105 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+
 
 class discussions extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ChatScreen(),
+      home: UserProfilePage(),
     );
   }
 }
 
-class ChatScreen extends StatefulWidget {
+class UserProfilePage extends StatefulWidget {
   @override
-  State createState() => ChatScreenState();
+  _UserProfilePageState createState() => _UserProfilePageState();
 }
 
-class ChatScreenState extends State<ChatScreen> {
-  TextEditingController _messageController = TextEditingController();
-  List<Message> _messages = [];
-  User? _user;
-  String _userPhotoURL = '';
-  String _userCity = '';
+class _UserProfilePageState extends State<UserProfilePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final DatabaseReference _messagesRef =
+  FirebaseDatabase.instance.reference().child('messages');
+
+  String userName = "";
+  String userEmail = "";
+  String userCity = "";
+  String userCountry = "";
+  String profileImageUrl = "";
+  String coverImageUrl = "";
 
   @override
   void initState() {
     super.initState();
-    _getUser();
-    _subscribeToMessages();
-  }
-  void _subscribeToMessages() {
-    FirebaseFirestore.instance.collection('messages').snapshots().listen((snapshot) {
-      _messages.clear(); // Efface les messages existants pour éviter la duplication
-      snapshot.docs.forEach((doc) {
-        // Analyse les données depuis Firestore et crée des objets Message
-        String senderId = doc['senderId'];
-        String senderName = doc['senderName'];
-        String text = doc['text'];
-
-        Message message = Message(senderId, senderName, text);
-        setState(() {
-          _messages.add(message);
-        });
-      });
-    });
+    _getUserInfo();
+    _getLocation();
   }
 
-  void _getUser() async {
-    FirebaseAuth auth = FirebaseAuth.instance;
-    User? user = auth.currentUser;
+  Future<void> _getUserInfo() async {
+    final User? user = _auth.currentUser;
 
     if (user != null) {
-      // Get user data from Firestore
-      DocumentSnapshot userSnapshot = await FirebaseFirestore.instance
-          .collection('users')
-          .doc(user.uid)
-          .get();
+      setState(() {
+        userName = user.displayName ?? "Unknown User";
+        userEmail = user.email ?? "Unknown Email";
+        profileImageUrl = user.photoURL ?? "";
+      });
+    }
+  }
 
-      // Get user location
+  Future<void> _getLocation() async {
+    try {
       Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high,
       );
 
-      // Get city from user's location
-      String cityName = await _getCityFromLocation(position);
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+        position.latitude,
+        position.longitude,
+      );
 
       setState(() {
-        _user = user;
-        _userPhotoURL = userSnapshot['photoURL'] ?? '';
-        _userCity = cityName;
+        userCity = placemarks.first.locality ?? "Unknown City";
+        userCountry = placemarks.first.country ?? "Unknown Country";
+      });
+    } catch (e) {
+      print("Error getting location: $e");
+    }
+  }
+
+  Future<void> _uploadImageToStorage(File image, String storagePath) async {
+    final User? user = _auth.currentUser;
+
+    if (user != null) {
+      final firebase_storage.Reference storageReference =
+      firebase_storage.FirebaseStorage.instance.ref().child(storagePath);
+      await storageReference.putFile(image);
+      String downloadURL = await storageReference.getDownloadURL();
+
+      setState(() {
+        if (storagePath == 'profile_images/') {
+          profileImageUrl = downloadURL;
+        } else if (storagePath == 'cover_images/') {
+          coverImageUrl = downloadURL;
+        }
       });
     }
   }
 
-  Future<Position> _getCurrentLocation() async {
-    return await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
-    );
-  }
+  Future<void> _pickImageAndUpload(String storagePath) async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
 
-  Future<String> _getCityFromLocation(Position position) async {
-    List<Placemark> placemarks = await placemarkFromCoordinates(
-      position.latitude,
-      position.longitude,
-    );
-
-    if (placemarks.isNotEmpty) {
-      return placemarks.first.locality ?? '';
-    } else {
-      return '';
+    if (image != null) {
+      _uploadImageToStorage(File(image.path), storagePath);
     }
   }
 
@@ -99,206 +107,65 @@ class ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: _user != null ? Text(_user!.displayName ?? '') : Text('COMMUNAUTE NO WAR'),
-        backgroundColor: Colors.blue,
-        leading: _user != null
-            ? IconButton(
-          icon: CircleAvatar(
-            backgroundImage:  AssetImage('assets/images/avatar.jpg'), // Placeholder image
-          ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => EditProfilePage(user: _user!)),
-            ).then((_) {
-              // Refresh the user data after returning from the edit profile page
-              _getUser();
-            });
-          },
-        )
-            : null,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.people),
-            onPressed: () {
-              // Handle displaying users in the same city
-            },
-          ),
-        ],
-        bottom: _user != null
-            ? PreferredSize(
-          preferredSize: Size.fromHeight(50.0),
-          child: Container(
-            padding: EdgeInsets.all(8.0),
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'Ville: $_userCity',
-              style: TextStyle(fontSize: 16.0),
-            ),
-          ),
-        )
-            : null,
+        title: Text('User Profile'),
       ),
-    body: Container(
-    color: Colors.white, // Définissez la couleur de fond sur le noir
-    child: Column(
-    children: [
-    Expanded(
-            child: ListView.builder(
-              itemCount: _messages.length,
-              itemBuilder: (context, index) {
-                return _buildMessage(_messages[index]);
-              },
-            ),
-          ),
-          _buildInputField(),
-        ],
-      ),
-    ));
-  }
-
-  Widget _buildMessage(Message message) {
-    bool isMyMessage = message.senderId == _user?.uid;
-    return Container(
-      alignment: isMyMessage ? Alignment.centerRight : Alignment.centerLeft,
-      child: Container(
-        padding: EdgeInsets.all(8.0),
-        margin: EdgeInsets.all(8.0),
-        decoration: BoxDecoration(
-          color: isMyMessage ? Colors.blue : Colors.white,
-          borderRadius: BorderRadius.circular(12.0),
-          image: DecorationImage(
-            image: AssetImage('assets/images/avatar.jpg'), // Placeholder image
-            fit: BoxFit.cover,
-          ),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              message.senderName,
-              style: TextStyle(fontWeight: FontWeight.bold),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: profileImageUrl.isNotEmpty
+                  ? NetworkImage(profileImageUrl)
+                  : null, // Utilisez null si l'URL est vide
+              child: profileImageUrl.isEmpty
+                  ? Image.asset(
+                'aassets/images/avatar.jpg',
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              )
+                  : null, // Utilisez null si l'URL n'est pas vide
             ),
-            Text(message.text),
-          ],
-        ),
-      ),
-    );
-  }
 
-  Widget _buildInputField() {
-    return Container(
-      padding: EdgeInsets.all(8.0),
-      child: Row(
-        children: [
-          Expanded(
-            child: TextField(
-              controller: _messageController,
-              decoration: InputDecoration(
-                hintText: 'Envoyer un message...',
-              ),
-            ),
-          ),
-          IconButton(
-            icon: Icon(Icons.send),
-            onPressed: () {
-              _sendMessage();
-            },
-          ),
-        ],
-      ),
-    );
-  }
 
-  void _sendMessage() {
-    String messageText = _messageController.text.trim();
-    if (messageText.isNotEmpty && _user != null) {
-      Message newMessage = Message(_user!.uid, _user!.displayName ?? '', messageText);
-      // Enregistre le message dans Firebase Firestore
-      FirebaseFirestore.instance.collection('messages').add({
-        'senderId': newMessage.senderId,
-        'senderName': newMessage.senderName,
-        'text': newMessage.text,
-        'timestamp': FieldValue.serverTimestamp(),
-      });
-      setState(() {
-        _messages.add(newMessage);
-        _messageController.clear();
-      });
-    }
-  }
-}
-
-class Message {
-  final String senderId;
-  final String senderName;
-  final String text;
-
-  Message(this.senderId, this.senderName, this.text);
-}
-
-class EditProfilePage extends StatefulWidget {
-  final User user;
-
-  const EditProfilePage({required this.user});
-
-  @override
-  _EditProfilePageState createState() => _EditProfilePageState();
-}
-
-class _EditProfilePageState extends State<EditProfilePage> {
-  TextEditingController _nameController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    // Fetch user data and populate the controllers
-    // You can use FirebaseAuth or any other method to get user data
-    _nameController.text = widget.user.displayName ?? '';
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Modifier le profil'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(labelText: 'Nom'),
-            ),
-            SizedBox(height: 16.0),
+            SizedBox(height: 16),
+            Text('Name: $userName'),
+            Text('Email: $userEmail'),
+            SizedBox(height: 16),
             ElevatedButton(
               onPressed: () {
-                _updateUserProfile();
+                _pickImageAndUpload('profile_images/');
               },
-              child: Text('Enregistrer'),
+              child: Text('Change Profile Picture'),
+            ),
+            SizedBox(height: 16),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: coverImageUrl.isNotEmpty
+                  ? NetworkImage(coverImageUrl)
+                  : null, // Utilisez null si l'URL est vide
+              child: coverImageUrl.isEmpty
+                  ? Image.asset(
+                'assets/images/avatar.jpg',
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              )
+                  : null, // Utilisez null si l'URL n'est pas vide
+            ),
+
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                _pickImageAndUpload('cover_images/');
+              },
+              child: Text('Change Cover Photo'),
             ),
           ],
         ),
       ),
     );
   }
-
-  void _updateUserProfile() async {
-    try {
-      await widget.user.updateDisplayName(_nameController.text);
-
-      // Update user data in Firestore
-      await FirebaseFirestore.instance
-          .collection('users')
-          .doc(widget.user.uid)
-          .update({'displayName': _nameController.text});
-
-      // Refresh the user data in the parent page
-      Navigator.pop(context, true); // Pass a result back to trigger refresh
-    } catch (e) {
-      print('Error updating user profile: $e');
-    // Handle the error as needed
-  }
-}
 }
